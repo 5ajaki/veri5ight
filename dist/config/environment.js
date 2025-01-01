3694ae132804f79378839898ae93d8516be83831
5ajaki <help>
2024-12-31 19:33:14 -0500

    No Build Errors

--- dist/config/environment.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-    ethereum: {
-        nodeUrl: process.env.ETHEREUM_NODE_URL || "http://nethermind.public.dappnode:8545",
-        ensContractAddress: "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72",
-    },
-};
